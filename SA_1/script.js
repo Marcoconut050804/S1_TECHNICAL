c95423ace518e127ea6ef2ad2700de29af5b7140
Marcoconut050804 <157201800+Marcoconut050804@users.noreply.github.com>
2024-01-20 15:53:32 +0800

    Add files via upload

---
+++ SA_1/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    function getRandomColor() {
+      const letters = "0123456789ABCDEF";
+      let color = "#";
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    }
+  
+    document.getElementById("changeColorBtn").addEventListener("click", function () {
+      const container = document.querySelector('.container');
+      container.style.backgroundColor = getRandomColor();
+    });
+  });
+  
